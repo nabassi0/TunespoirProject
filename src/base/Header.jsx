2b277a32275ad5c352bfb7d061acbef001c23cf8
trimize <teorimize7@gmail.com>
2025-05-10 16:06:47 +0200

    Fix
    - index.html was missing
    - landing page was missing

---
+++ src/base/Header.jsx
@@ -0,0 +1,12 @@
+import React, { useContext, useEffect, useRef, useState } from 'react';
+
+const Header = () => {
+
+	return (
+        <div className='header-container'>
+
+        </div>
+	);
+};
+
+export default Header;
---
+++ src/base/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React, { useContext, useEffect, useRef, useState } from 'react';
+
+const HomePage = () => {
+
+    return (
+        <div className='home-page-container'>
+            
+        </div>
+    );
+};
+
+export default HomePage;
