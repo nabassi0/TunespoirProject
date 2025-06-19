f5a48f0d71491a2ab8b809f137aaa0360f2a2af1
naier abassi <naierabassi@gmail.com>
2025-06-19 12:30:34 +0200

    fixing css and actualites

--- src/components/home/NewsSection/NewsSection.jsx
+++ src/components/home/NewsSection/NewsSection.jsx
@@ -10,49 +10,52 @@ import EcoleHabibBourguiba from "../../../assets/images/ecole_habibBourgiba.png"
 /**
  * News card component
  * @param {Object} props - Component props
- * @param {string} props.image - URL for the news image
- * @param {string} props.imageAlt - Alt text for the news image
- * @param {string} props.date - Publication date
- * @param {string} props.title - News title
- * @param {string} props.excerpt - Short description/excerpt
- * @param {string} props.link - Link to the full article
- * @param {string} props.linkText - Text for the link
+ * @param {Object} props.newsItem - News item object
  * @returns {JSX.Element}
  */
-const NewsCard = ({
-  image,
-  imageAlt,
-  date,
-  title,
-  excerpt,
-  link,
-  linkText,
-}) => (
-  <div className="news-card">
-    <img src={image} alt={imageAlt} className="news-image" />
-    <div className="news-content">
-      <span className="news-date">{date}</span>
-      <h3 className="news-title">{title}</h3>
-      <p className="news-excerpt">{excerpt}</p>
-      <Link to={link} className="read-more">
-        {linkText} <FaArrowRight className="icon-arrow" />
-      </Link>
+const NewsCard = ({ newsItem }) => (
+  <Link to={newsItem.link} className="news-card">
+    <div className="news-image-container">
+      <img
+        src={newsItem.image}
+        alt={newsItem.imageAlt}
+        className="news-image"
+      />
+      <div className="news-overlay"></div>
+      <div className="news-category-badge">Éducation</div>
     </div>
-  </div>
+
+    <div className="news-content">
+      <div className="news-meta">
+        <span className="news-date">{newsItem.date}</span>
+        {newsItem.location && (
+          <span className="news-location">{newsItem.location}</span>
+        )}
+      </div>
+
+      <h3 className="news-title">{newsItem.title}</h3>
+      <p className="news-excerpt">{newsItem.excerpt}</p>
+
+      <div className="news-footer">
+        <span className="read-more">
+          {newsItem.linkText || "Lire l'article"}
+        </span>
+      </div>
+    </div>
+  </Link>
 );
 
 NewsCard.propTypes = {
-  image: PropTypes.string.isRequired,
-  imageAlt: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  excerpt: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
-  linkText: PropTypes.string,
-};
-
-NewsCard.defaultProps = {
-  linkText: "Lire l'article",
+  newsItem: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    imageAlt: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    excerpt: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    location: PropTypes.string,
+    linkText: PropTypes.string,
+  }).isRequired,
 };
 
 /**
@@ -77,6 +80,7 @@ const NewsSection = ({
       excerpt:
         "Notre équipe a réalisé la rénovation de l'école El Izdihar avec une reconstruction de près de 70% de l'école.",
       link: "/actualites",
+      location: "Fernana, Jendouba",
     },
     {
       image: EcoleJendouba,
@@ -86,15 +90,17 @@ const NewsSection = ({
       excerpt:
         "Grâce à vos dons, nous avons pu construire et équiper une école qui accueillera plus de 200 enfants.",
       link: "/actualites",
+      location: "Jendouba",
     },
     {
       image: EcoleHabibBourguiba,
-      imageAlt: "Rénonvation",
+      imageAlt: "Rénovation",
       date: "15 mai 2023",
       title: "Rénovation de l'école Habib Bourguiba à Jendouba",
       excerpt:
-        "Rénovation de l'éblissement en forte dégradation avec 14 salles de classes accueillant 750 élèves.",
+        "Rénovation de l'établissement en forte dégradation avec 14 salles de classes accueillant 750 élèves.",
       link: "/actualites",
+      location: "Jendouba",
     },
   ],
 }) => {
@@ -103,22 +109,14 @@ const NewsSection = ({
       <div className="container">
         <div className="section-header">
           <h2 className="section-title">{title}</h2>
+          <Link to={viewAllLink} className="view-all">
+            {viewAllText} <FaArrowRight />
+          </Link>
         </div>
-        <Link to={viewAllLink} className="view-all">
-          {viewAllText} <FaArrowRight />
-        </Link>
+
         <div className="news-grid">
           {newsItems.map((item, index) => (
-            <NewsCard
-              key={index}
-              image={item.image}
-              imageAlt={item.imageAlt}
-              date={item.date}
-              title={item.title}
-              excerpt={item.excerpt}
-              link={item.link}
-              linkText={item.linkText}
-            />
+            <NewsCard key={index} newsItem={item} />
           ))}
         </div>
       </div>
@@ -138,6 +136,7 @@ NewsSection.propTypes = {
       title: PropTypes.string.isRequired,
       excerpt: PropTypes.string.isRequired,
       link: PropTypes.string.isRequired,
+      location: PropTypes.string,
       linkText: PropTypes.string,
     })
   ),
